c53e7a6601edd4b8f412bab100d109879f96ba17
alcaloke <35482173+alcaloke@users.noreply.github.com>
2023-02-12 07:06:29 +0900

    Add files via upload

--- 06_circles/sketch.js
+++ 06_circles/sketch.js
@@ -5,55 +5,63 @@ function setup() {
 }
 
 let r = 0
-let p1ch = 195
-let dp1c = 0.7
+let p3cho = 155
+let p3ch = p3cho
+let dp3c = 0.7
 let d = 0
 let dx
+
 function draw() {
   background(0)
 
-  n = 41
-  s = 15
-  c = 15
+  n = 36
+  s = 18
+  c = 17
   w = (s * (n - 1)) / 2
   let dz = map(mouseY, height, 0, -10, 20)
 
   // C. Colors
   push()
   colorMode(HSB, 360)
-  let p1c = color(p1ch, 360, 360)
-  let p3c = color(p1ch + 100, 360, 360)
-  p1ch += dp1c
-  if (p1ch > 205 || p1ch < 195) {
-    dp1c *= -1
+
+  let p1c = color(245, 360, 360)
+
+  let p3c = color(p3ch, 360, 360)
+  p3ch += dp3c
+  if (p3ch > 185 || p3ch < p3cho) {
+    dp3c *= -1
   }
   pop()
 
   // B. Lights
-  // light 1
+  // light 1 - blue @ center
   pointLight(p1c, 0, 0, dz)
 
-  // light 2
+  // light 2 - white light in orbit
   let p2x = (w / 2) * sin(radians(r))
   let p2y = (w / 2) * cos(radians(r))
   pointLight(255, 255, 255, p2x, p2y, dz)
   push()
-  rotateZ(radians(-r))
-  translate(0, w / 2, 0)
+  translate(p2x, p2y, 0)
   noStroke()
   sphere(25)
   pop()
 
-  // light 3
+  // light 3 - fluctuating blue/green in orbit
   let p3x = (w / 2) * sin(radians(r - 180))
   let p3y = (w / 2) * cos(radians(r - 180))
   pointLight(p3c, p3x, p3y, dz)
   push()
-  rotateZ(radians(-r + 180))
-  translate(0, w / 2, 0)
+  translate(p3x, p3y, 0)
   noStroke()
   sphere(25)
   pop()
+
+  // spotLight [4] - blue light in 8 orbit
+  let f = 0.05
+  let p4x = f * sin(radians(-r * 2))
+  let p4y = f * 5 * cos(radians(r))
+  spotLight(0, 0, 255, 0, 0, 1000, p4x, p4y, -1)
 
   // D. plane
   push()
@@ -75,7 +83,7 @@ function draw() {
       rotateX(radians(ra + r))
       noStroke()
       specularMaterial(255)
-      shininess(50)
+      shininess(255)
       circle(0, 0, c)
       pop()
     }
